3f148033bca91bd53950d7e7be5972f17c1aafb8
Tirtha Ghosh <ghsoshtirtha1234@gmail.com>
2025-06-27 01:27:05 +0530

    Modifying the code

--- Function_of_Python/Arguments.py
+++ Function_of_Python/Arguments.py
@@ -9,7 +9,7 @@ average(b=3,a=1)
 
 # Required argument :- in case we dont pass the argument with a key= value syntax thenit is nessary to pass the argument in the currect positional order and the number of arguments pass shuld match the actual function definition 
 def avg3(a,b,c=2):
-  print("the average of the sum is = ",(a+b+c)/3)
+  print("The Average of the sum is = ",(a+b+c)/3)
 
 avg3(3,4)
 
