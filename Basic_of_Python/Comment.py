7200daaf62e1e44f3d98eb61aed48c7de724e232
Tirtha Ghosh <ghsoshtirtha1234@gmail.com>
2025-06-27 20:23:02 +0530

    Modifing by me

--- Basic_of_Python/Comment.py
+++ Basic_of_Python/Comment.py
@@ -7,4 +7,4 @@ ndhqwkdbiphqw hwqghj wqhjgeygi
 """
 # I know you are good , i am also good 
 print("Tirtha",5, "MT-15",end="000",sep="~")
-print("Hey how are you\" ? I know you are bd and i am also good  . tell  me what about your result in last sem ")
+print("Hey how are you\" ? I know you are bad boy and i am also good  . tell  me what about your result in last sem ")
